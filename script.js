48374020a276f99a667a6ef315e4f1f1211aa8b1
deondaci <deondaci1@gmail.com>
2024-03-05 06:27:19 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+a = Math.PI
+console.log(a)
+//numri pi
+
+b = Math.abs(-12)
+console.log(b)
+//vlera absolute
+
+c = Math.log(2)
+console.log(c)
+//logaritm
+
+d = Math.round(4.65)
+console.log(d)
+//rumbullaksim
+
+e = Math.sqrt(16)
+console.log(e)
+//rraanja katrore
+f = Math.trunc(7.34)
+console.log(f)
+//hek numrat mas presjes
+
+//dtr
+let koha
+koha = new Date()
+console.log(koha)
+// let koha2
+// koha2 = new Date(2023,11,14,16,24,32)
+// console.log(koha2)
+
+///
+
+// var detyr = setInterval(tra,600)
+// function tra(){
+//     alert('Setinterval')
+// }
+
+// var count = 0
+// var drta = setInterval(kro, 1000)
+// function kro() {
+//     console.log(count)
+//     count++;
+//     if (count == 10) {
+//         clearInterval(drta)
+//     }
+// }
+
+//SetTimeout()-----ja cakton 1 kohe edhe veq 1 he
+
